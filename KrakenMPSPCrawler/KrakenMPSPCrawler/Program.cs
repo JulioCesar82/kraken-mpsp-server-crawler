b02559c328bb260b52b9f31b6d4a0d64afe3f422
julio avila <juliocesarde.avila82@gmail.com>
2019-08-04 17:38:14 -0300

    criado projeto base do crawler

---
+++ KrakenMPSPCrawler/KrakenMPSPCrawler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KrakenMPSPCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
