000c437dabe573ff006713499b400b01156ab97e
gama595 <44736790+gama595@users.noreply.github.com>
2019-10-02 21:06:33 -0300

    Adicionado o Model do DetranCrawler

---
+++ KrakenMPSPBusiness/KrakenMPSPBusiness/Models/DetranCrawlerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KrakenMPSPBusiness.Models
+{
+    public class DetranCrawlerModel
+    {
+        public long Id { get; set; }
+        public string Renach { get; set; }
+        public string Categoria { get; set; }
+        public string Emissao { get; set; }
+        public string Nascimento { get; set; }
+        public string Nome { get; set; }
+        public string NomePai { get; set; }
+        public string NomeMae { get; set; }
+        public string Registro { get; set; }
+        public string Tipografo { get; set; }
+        public string Identidade { get; set; }
+    }
+}
